621c34901f8f9a9137b9bec592c2cbd1a6cb23f7
PeterIceking <peterice@163.com>
2014-03-10 23:47:34 +0800

    增加fruit_types后台管理的CRUD

--- app/controllers/admin/fruit_type_controller.rb
+++ app/controllers/admin/fruit_type_controller.rb
@@ -1,25 +1,64 @@
 # -*- encoding: utf-8 -*-
 class Admin::FruitTypeController < ApplicationController
+include ApplicationHelper
   def new
 		@fruit_type = FruitType.new
-		@page_title = "新建用户"
+		@page_title = "类别管理"
   end
 
-  def creat
+  def create
+		if request.post? and params[:fruit_type]
+			@fruit_type = FruitType.new(fruit_type_params)
+			if @fruit_type.save
+				flash[:notice] = fruit_type_params[:type_name].to_s #"添加成功"
+				redirect_to :action => "index"
+			else
+				flash[:alert] = errors_for(@fruit_type, "抱歉，请检查输入信息：").html_safe
+				redirect_to :action => "new"
+			end
+		end
   end
 
   def edit
+		@fruit_type = FruitType.find(params[:id])
+		@page_title = @fruit_type.type_name
   end
 
   def update
+		if request.post? and params[:fruit_type]
+			@fruit_type = FruitType.find(params[:id])
+			if @fruit_type.update_attributes(fruit_type_params)
+				flash[:notice] = fruit_type_params[:type_name].to_s #"更新成功"
+				redirect_to :action => "index"
+			else
+				flash[:alert] = errors_for(@fruit_type, "抱歉，请检查输入信息：").html_safe
+				render :action => "edit"
+			end
+		end
   end
 
   def destroy
+		if request.post? and params[:id]
+			@fruit_type = FruitType.find(params[:id])
+			flash[:notice] = "成功删除：#{@fruit_type.type_name}" #"删除成功"
+			@fruit_type.destroy
+			# flash[:alert] = errors_for(@fruit_type, "抱歉，请检查输入信息：").html_safe
+			redirect_to :action => "index"
+		end
   end
 
   def show
+		@fruit_type = FruitType.find(params[:id])
+		@page_title = @fruit_type.type_name
   end
 
   def index
+		@fruit_types = FruitType.all
+		@page_title = "类别"
   end
+	
+	private
+	def fruit_type_params
+		params.require(:fruit_type).permit(:type_name,:type_description,:super_type_id)
+	end
 end
--- db/migrate/20140310052814_create_fruit_types.rb
+++ db/migrate/20140310052814_create_fruit_types.rb
@@ -3,7 +3,7 @@ class CreateFruitTypes < ActiveRecord::Migration
     create_table :fruit_types do |t|
 			t.string		:type_name,
 									:type_description
-			t.integer		:super_tpye_id
+			t.integer		:super_type_id
       t.timestamps
     end
   end
